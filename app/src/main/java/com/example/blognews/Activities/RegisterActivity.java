daf08b425bd20b9a48039c3aaedb9f14a2d1ee70
ntkiet99 <ntkiet99.it@gmail.com>
2020-11-08 18:27:48 +0700

    First commit

---
+++ app/src/main/java/com/example/blognews/Activities/RegisterActivity.java
@@ -0,0 +1,188 @@
+package com.example.blognews.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.blognews.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ImageView ImgUserPhoto;
+    static int PREQCODE = 1;
+    static int REQUESCODE = 1;
+    static boolean checkImageChange = true;
+    Uri pickedImgUri;
+    private EditText userName, userEmail, userPassword, userConfirmPassword;
+    private ProgressBar loadingProgress;
+    private Button regBtn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        userEmail = findViewById(R.id.regMail);
+        userName = findViewById(R.id.regName);
+        userPassword = findViewById(R.id.regPassword);
+        userConfirmPassword = findViewById(R.id.regComfirmPassword);
+        loadingProgress = findViewById(R.id.regProgressBar);
+        regBtn = findViewById(R.id.regBtn);
+
+        loadingProgress.setVisibility(View.INVISIBLE);
+        mAuth = FirebaseAuth.getInstance();
+        ImgUserPhoto = findViewById(R.id.regUserPhoto);
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(Build.VERSION.SDK_INT >= 22)
+                {
+                    checkAndRequestForPermission();
+                }
+                else{
+                    openGallery();
+                }
+            }
+        });
+
+        regBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                regBtn.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                final String password2 = userConfirmPassword.getText().toString();
+                final String name = userName.getText().toString();
+
+                if(checkImageChange){
+                    showMessage("Please Enter Image of you");
+
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility((View.INVISIBLE));
+                }else{
+                    if(email.isEmpty() || name.isEmpty() || password.isEmpty() || !password.equals(password2) ){
+                        showMessage("Please Verify all fields");
+
+                        regBtn.setVisibility(View.VISIBLE);
+                        loadingProgress.setVisibility((View.INVISIBLE));
+                    }else{
+                        CreateUserAccount(email, name, password);
+                    }
+                }
+            }
+        });
+    }
+
+    private void CreateUserAccount(String email, String name, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    showMessage("Account created");
+                    updateUserInfo(name, pickedImgUri, mAuth.getCurrentUser());
+                }else {
+                    showMessage("Account creation failed" + task.getException().getMessage());
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+    }
+
+    private void updateUserInfo(final String name, Uri pickedImgUri,final FirebaseUser currentUser) {
+        StorageReference mStoage = FirebaseStorage.getInstance().getReference().child("users_photos");
+        Long tsLong = System.currentTimeMillis() / 1000;
+        String ts = tsLong.toString();
+        StorageReference imageFilePath = mStoage.child("image-"+ ts + ".jpg");
+        imageFilePath.putFile(pickedImgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder().setDisplayName(name).setPhotoUri(uri).build();
+                        currentUser.updateProfile(profileUpdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()) {
+                                    showMessage("Register Complete");
+                                    checkImageChange = true;
+                                    updateUI();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+
+        });
+
+    }
+
+    private void updateUI() {
+        Intent homeActivity = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(homeActivity);
+        finish();
+    }
+
+    private  void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    private  void checkAndRequestForPermission() {
+        if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(RegisterActivity.this, "Please accept for required permission", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                ActivityCompat.requestPermissions(RegisterActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PREQCODE);
+            }
+        } else {
+            openGallery();
+        }
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQUESCODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == REQUESCODE && data != null) {
+            pickedImgUri = data.getData();
+            ImgUserPhoto.setImageURI(pickedImgUri);
+            checkImageChange = false;
+        }
+    }
+}
